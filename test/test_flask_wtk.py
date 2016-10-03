a720302b8418c613e59ef4d03b3a425e82a923ca
Harry Sorensen <harry.sorensen@nrel.gov>
2016-10-03 14:32:14 -0600

    Add test for fcst flask

--- test/test_flask_wtk.py
+++ test/test_flask_wtk.py
@@ -1,7 +1,9 @@
 import json
+import numpy
 import pandas
 from unittest import TestCase, skip
 import flask_wtk
+from wtk_api import FORECAST_ATTRS
 
 class TestFlaskWTK(TestCase):
     def setUp(self):
@@ -18,6 +20,58 @@ class TestFlaskWTK(TestCase):
         resp = self.app.get(req)
         resp_data = resp.get_data()
         ret_data = json.loads(resp_data)
+        self.assertIn(site_id, ret_data)
+        df = pandas.read_json(ret_data[site_id])
+        first_row = df.ix[0].to_dict()
+        for n,v in expected_dict.items():
+            self.assertAlmostEqual(v, first_row[n])
+        #self.assertEqual(expected_dict, df.ix[0].to_dict())
+        self.assertEqual(14*24*12+1, len(df)) # End is inclusive of midnight
+
+
+    def test_fcst(self):
+        site_id = "53252"
+        start = pandas.Timestamp('2007-01-01', tz='utc')
+        end = pandas.Timestamp('2007-01-02', tz='utc')
+        expected = numpy.array([6.2671943, 8.6079865, 6.7353525,
+                    6.384234, 0.26309761, 3.6874273, 1.4196928, 0.53551841,
+                    10.572015, 13.249797, 10.526829, 10.306773], dtype='float32')
+        ex_dict = dict(zip(FORECAST_ATTRS, expected))
+        req = '/fcst?site_id=%s&start=%s&end=%s'%(site_id,start.value//10**9, end.value//10**9)
+        #print "Request is %s"%req
+        resp = self.app.get(req)
+        resp_data = resp.get_data()
+        ret_data = json.loads(resp_data)
+        self.assertIn(site_id, ret_data)
+        fcst_data = pandas.read_json(ret_data[site_id])
+        self.assertEqual(25, len(fcst_data))
+        for k, v in ex_dict.items():
+            self.assertAlmostEqual(v, fcst_data.ix[0][k])
+        return
+
+
+        start = pandas.Timestamp('2007-01-01', tz='utc')
+        end = pandas.Timestamp('2007-01-02', tz='utc')
+        fcst_data = get_forecast_data("53252", start, end, utc=True)
+        self.assertEqual(start, fcst_data.index[0])
+        # From ncdump, all values are float32 which do not compare easily to
+        # python floats which are float64
+        expected = numpy.array([6.2671943, 8.6079865, 6.7353525,
+                    6.384234, 0.26309761, 3.6874273, 1.4196928, 0.53551841,
+                    10.572015, 13.249797, 10.526829, 10.306773], dtype='float32')
+        self.assertEqual(25, len(fcst_data))
+        self.assertTrue(numpy.array_equal(expected, list(fcst_data.ix[0])))
+        ex_dict = dict(zip(FORECAST_ATTRS, expected))
+        # Verify column names are correct
+        for k, v in ex_dict.items():
+            self.assertEqual(v, fcst_data.ix[0][k])
+
+
+
+
+
+
+
         self.assertIn(site_id, ret_data)
         df = pandas.read_json(ret_data[site_id])
         first_row = df.ix[0].to_dict()
